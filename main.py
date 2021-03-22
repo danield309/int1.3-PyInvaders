4ae456e49b834c208e557aba307d20ae6c2ed4a5
Daniel Duque <duque1daniel@gmail.com>
2021-03-21 21:15:06 -0700

    [ADD] test run

--- main.py
+++ main.py
@@ -24,3 +24,16 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 
 # Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
+
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+main()
